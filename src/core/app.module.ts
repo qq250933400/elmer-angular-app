d238d40b90cea80558d71b110512d582c48637e5
qq250933400 <250933400@qq.com>
2017-10-20 22:24:41 +0800

    fix router bug

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -5,18 +5,18 @@ import { RouterModule }   from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpModule }    from '@angular/http';
 
-import ConfigRouters, { AppDeclarations } from './route.config';
+import { AppDeclarations, ConfigRouters } from './route.config';
 import { AppComponent } from './app.component';
 
 import { AppService } from '../services/app.service';
+
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        RouterModule.forRoot([
-            ...ConfigRouters
-        ])
+        RouterModule.forRoot(ConfigRouters)
     ],
     declarations: [
         AppComponent,
--- src/core/route.config.ts
+++ src/core/route.config.ts
@@ -1,11 +1,13 @@
+import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
 import { LoginComponent } from '../app/login/login.component';
+
 export const AppDeclarations = [
     HomeComponent,
     LoginComponent
 ];
 
-export default [
+export const ConfigRouters: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent }
